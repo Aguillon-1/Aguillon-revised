b120ea41e9568355a40925e063247551bb2dda54
Daniel Bobis <bobis.daniel.bscs2023@gmail.com>
2025-05-25 16:01:25 +0800

    Login form to Main form linked

--- User Interface/MainForm.cs
+++ User Interface/MainForm.cs
@@ -1,20 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CMS_Revised.User_Interface
 {
     public partial class MainForm : Form
     {
-        public MainForm()
+        private int _userId;
+        private string _userEmail;
+        private string _userName;
+
+        public MainForm(int userId, string userEmail, string userName)
         {
             InitializeComponent();
+            _userId = userId;
+            _userEmail = userEmail;
+            _userName = userName;
+
+            // Make sure MainForm_Load is hooked up
+            this.Load += MainForm_Load;
+
+            // Ensure the app closes when MainForm is closed
+            this.FormClosed += (s, e) => Application.Exit();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Make sure 'home1' is the actual name of your Home user control instance
+            home1.SetUserInfo(_userId, _userEmail, _userName);
         }
     }
 }
