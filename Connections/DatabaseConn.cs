581cf2edf00f8ebd01dc02f895d8821786e7c0cd
Daniel Bobis <bobis.daniel.bscs2023@gmail.com>
2025-05-31 10:46:33 +0800

    updated directory for db (ignore this)

--- Connections/DatabaseConn.cs
+++ Connections/DatabaseConn.cs
@@ -7,7 +7,7 @@ namespace CMS_Revised.Connections
     public class DatabaseConn
     {
         // LocalDB connection string for Database1.mdf in the project directory
-        private const string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\danie\source\repos\CMS_Revised\Connections\Database1.mdf;Integrated Security=True;Connect Timeout=30";
+        private const string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\danie\source\repos\Bobsi01\CMS_Revised\Connections\Database1.mdf;Integrated Security=True;Connect Timeout=30";
 
         public static SqlConnection GetConnection()
         {
