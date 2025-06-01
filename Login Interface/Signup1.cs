325d552fc9c65369ef3623d3d693534d6d46f457
Daniel Bobis <bobis.daniel.bscs2023@gmail.com>
2025-06-01 18:49:02 +0800

    Debugging dialogue enchanced, login interface files organized

---
+++ Login Interface/Signup1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMS_Revised.Login_Interface
+{
+    public partial class Signup1 : UserControl
+    {
+        public Signup1()
+        {
+            InitializeComponent();
+        }
+    }
+}
