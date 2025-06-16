5b4a7d632afac970a33d374c511e9f3ba7f64c80
ItsMeYourNightmare <149944017+Bobsi01@users.noreply.github.com>
2025-06-16 15:45:55 +0800

    Update DatabaseConn.cs
    ONLINE DATABASE IS UP

--- Connections/DatabaseConn.cs
+++ Connections/DatabaseConn.cs
@@ -7,7 +7,8 @@ namespace CMS_Revised.Connections
     public class DatabaseConn
     {
         // LocalDB connection string for Database1.mdf in the project directory
-        private const string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\danie\source\repos\Bobsi01\CMS_Revised\Connections\Database1.mdf;Integrated Security=True;Connect Timeout=30";
+        //private const string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\danie\source\repos\Bobsi01\CMS_Revised\Connections\Database1.mdf;Integrated Security=True;Connect Timeout=30";
+        private const string ConnectionString = @"Server=tcp:cmsrevised2526.database.windows.net,1433;Initial Catalog=CMS-revised-DB;Persist Security Info=False;User ID=cmsrevised2526;Password=CMSrevised25;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         public static SqlConnection GetConnection()
         {
